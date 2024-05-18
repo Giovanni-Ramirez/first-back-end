f50315cd877c193353811573c527d167f3a23a5f
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-05-18 11:20:14 -0400

    added uuid from npm and complete delete fetch call

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,9 @@
 const notes = require('express').Router();
 const fs = require('fs');
 const db = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
+// This gets the notes list (db) then displays on the left
 notes.get('/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -11,15 +13,16 @@ notes.get('/notes', (req, res) => {
         }
     })
 });
-
+// This add a note to the note list (db)
 notes.post('/notes', (req, res) => {
     
-   const { title, text } = req.body;
-
-   if(title&&text) {
+    const { title, text } = req.body;
+    const id = uuidv4();
+    if(title&&text&&id) {
     const newTask = {
+        id,
         title,
-        text
+        text,
     };
 
     fs.readFile('db/db.json', 'utf-8', (err, data) => {
@@ -54,8 +57,43 @@ notes.post('/notes', (req, res) => {
     res.status(500).json('Error in posting review');
   }
 });
+// This deletes a note from the db and updates the db
+notes.delete('/notes/:id', (req, res) => {
+    //The id we are looking for
+    const noteId = req.params.id;
+    
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        console.log('starting fs.readfile');//testing
+        if (err) {
+            console.log(err);
+        } else {
+            //The parsed db in an array
+            const parsedTasks = JSON.parse(data);
 
+            //looping throught an array to find the matching id
+            for (let i = 0; i < parsedTasks.length; i++) {
+                if(parsedTasks[i].id == noteId) {
+                    //deleteing the element with the same id
+                    parsedTasks.splice(i, 1);
+                    //overwriting the file location with updated parsedTask array
+                    fs.writeFile(
+                        'db/db.json',
+                        JSON.stringify(parsedTasks, null, 2),
+                        (writeErr) =>
+                            writeErr
+                              ? console.error(writeErr)
+                              : console.info('Successfully updated tasks db')
+                    );
+                }
+            }
+        }
+    });
 
+    const response = 'Deleted note and updated the notes list!'
+
+    console.log(response);
+    res.status(201).json(response);
+});
 
 
 
--- server.js
+++ server.js
@@ -2,26 +2,27 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/apiRoutes.js');
 
-const PORT = process.env.port || 3001;;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
 // middle wear
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // any get starting with /api will get sent to route
 app.use('/api', api);
 
-app.use(express.static('public'));
-
+// on load will route to the main index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-
+// On the click of button will take to this html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+// listen for any call on this port
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
