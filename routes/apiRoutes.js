4bb69a7f23d74b1fec1577de13ec0f4d6779fe15
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-05-17 21:45:09 -0400

    fixed a bug in /notes GET and finished /notes POST

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,57 @@ const fs = require('fs');
 const db = require('../db/db.json');
 
 notes.get('/notes', (req, res) => {
-    res.json(db)
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(JSON.parse(data))
+        }
+    })
 });
 
 notes.post('/notes', (req, res) => {
+    
+   const { title, text } = req.body;
 
-})
+   if(title&&text) {
+    const newTask = {
+        title,
+        text
+    };
+
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        console.log('readfile was ran');
+        if (err) {
+            console.log(err);
+        } else {
+            const parsedTasks = JSON.parse(data);
+
+            parsedTasks.push(newTask);
+
+            fs.writeFile(
+                'db/db.json',
+                JSON.stringify(parsedTasks, null, 2),
+                (writeErr) =>
+                    writeErr
+                      ? console.error(writeErr)
+                      : console.info('Successfully updated tasks db')
+            );
+
+        }
+    });
+
+    const response = {
+        status: 'success',
+        body: newTask,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+   } else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 
 
--- routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-const notes = require('express').Router();
-
-const fs = require('fs');
-
-notes.get('/', (req, res) => {
-    fs.readFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
-});
-
-module.exports = notes;
